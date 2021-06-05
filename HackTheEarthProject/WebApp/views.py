77c14aef509aa0a5c81a45033292f6d0ebd70716
Krish <kahnscatterkrish@gmail.com>
2021-06-04 22:31:40 -0400

    Made simple login

--- HackTheEarthProject/WebApp/views.py
+++ HackTheEarthProject/WebApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from WebApp.forms import *
+from WebApp.models import *
 
 # Create your views here.
 
@@ -6,6 +8,25 @@ def index(request):
 	return render(request, "WebApp\\index.html")
 
 def login(request):
-	return render(request, "WebApp\\login.html", {
-		"loginForm"
-	})
+    errorMessage = ''
+    if request.method == 'POST':
+        user_input = loginForms(request.POST)
+        if user_input.is_valid():
+            entered_username = user_input.cleaned_data["username"]
+            entered_password = user_input.cleaned_data["password"]
+            if Users.objects.filter(username=entered_username):
+                if Users.objects.filter(username=entered_username).filter(password=entered_password):
+                    request.session['username'] = entered_username
+                    request.session['loged_in'] = True
+                    return redirect(index)
+                else:
+                    request.session['loged_in'] = False
+                    errorMessage = 'Wrong password'
+            else:
+                request.session['loged_in'] = False
+                errorMessage = 'No such account'
+
+    return render(request, "WebApp\\login.html", {
+        'loginForm': loginForms(),
+        'errorMessage': errorMessage
+    })
