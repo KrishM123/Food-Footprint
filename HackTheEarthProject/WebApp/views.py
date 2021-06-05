29ed610854142657c5943b53631bbb48544fe095
Krish <kahnscatterkrish@gmail.com>
2021-06-04 21:24:07 -0400

    Created Login

--- HackTheEarthProject/WebApp/views.py
+++ HackTheEarthProject/WebApp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "WebApp\\index.html")
+
+def login(request):
+	return render(request, "WebApp\\login.html", {
+		"loginForm"
+	})
